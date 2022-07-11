73623fae2c0d26604e8ec7b6152731e187e84e16
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-07-12 00:14:44 +0800

    feat: rename the folder 'controllers' as new name 'utils'

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const { create, findById } = require('../models/url')
 const router = express.Router()
 const Url = require('../models/url')
-// const domain = 'https://afternoon-mountain-84879.herokuapp.com/' || `localhost:3000/`
-const domain = `localhost:3000/`
+const urlShortner = require('../utils/url_shortner')
+const domain = 'https://afternoon-mountain-84879.herokuapp.com/' || `localhost:3000/`
+// const domain = `localhost:3000/`
 
 const lowercase = 'abcdefghijklmnopqrstuvwxyz'
 const uppercase = lowercase.toUpperCase()
@@ -11,14 +12,14 @@ const number = '0123456789'
 const mix = lowercase.split('').concat(uppercase.split(''), number.split('')) // 此陣列長度為 62
 
 // 隨機五碼英數組合產出函式
-function randomCode(digit) {
-  let finalCode = ''
-  for (let i = 1; i < digit + 1; i++) {
-    const oneCode = Math.floor(Math.random() * 62)
-    finalCode += mix[oneCode]
-  }
-  return finalCode
-}
+// function randomCode(digit) {
+//   let finalCode = ''
+//   for (let i = 1; i < digit + 1; i++) {
+//     const oneCode = Math.floor(Math.random() * 62)
+//     finalCode += mix[oneCode]
+//   }
+//   return finalCode
+// }
 
 // 一鍵複製函式（開發中）
 
@@ -38,7 +39,7 @@ router.post('/', (req, res) => {
       const target = dataArray.find(oneObject => oneObject.url === url)
       if (target === undefined) {
         // 上一行用 find() 檢視資料庫內有無縮址記錄。若無則在資料庫建立一筆新的縮址記錄。
-        const code = randomCode(5)
+        const code = urlShortner(5)
         return Url.create({ url, code })
           .then(() => res.render('show', { domain, code }))
       } else {
@@ -62,7 +63,5 @@ router.get('/:code', (req, res) => {
       res.redirect(`${oneObject.url}`)
     })
 })
-
-router.get
 
 module.exports = router
